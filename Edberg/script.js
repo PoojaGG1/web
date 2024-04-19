6e0057cb4751d18d9474e3b0ca17202135c2ed9d
Pooja <poojanisha2000@gmail.com>
2024-04-19 13:52:13 +0530

    timer

--- Edberg/script.js
+++ Edberg/script.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,15 +12,18 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
+
+// Automatic slideshow (optional)
+let timer = setInterval(() => plusSlides(1), 2000); // Change 3000 to adjust time between slides in milliseconds
+
+// Stop automatic slideshow on hover (optional)
+let slideContainer = document.querySelector('.slideshow-container');
+slideContainer.addEventListener('mouseover', () => clearInterval(timer));
+slideContainer.addEventListener('mouseout', () => timer = setInterval(() => plusSlides(1), 2000)); 
