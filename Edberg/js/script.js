0f0007c0fa4b5d5869d2d5b593efddaddc991614
Pooja <poojanisha2000@gmail.com>
2024-04-22 16:16:19 +0530

    learner benefits

--- Edberg/js/script.js
+++ Edberg/js/script.js
@@ -1,6 +1,34 @@
 const menuBtn = document.getElementById('menu-btn');
 const navLink = document.getElementById('nav-links');
 
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Automatic slideshow (optional)
+let timer = setInterval(() => plusSlides(1), 2000); // Change 3000 to adjust time between slides in milliseconds
+
+// Stop automatic slideshow on hover (optional)
+let slideContainer = document.querySelector('.slideshow-container');
+slideContainer.addEventListener('mouseover', () => clearInterval(timer));
+slideContainer.addEventListener('mouseout', () => timer = setInterval(() => plusSlides(1), 2000)); 
+
 menuBtn.addEventListener('click', () => {
     navLink.style.display = navLink.style.display === 'none' ? 'flex' : 'none';
 });
