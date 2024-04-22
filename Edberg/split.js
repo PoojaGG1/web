5c57c78b1d95703e716f906d90837b1c818d45ec
Pooja <poojanisha2000@gmail.com>
2024-04-22 11:26:01 +0530

    Co-authored-by: pranjali8112002 <pranjali8112002@users.noreply.github.com> Co-authored-by: sejalpore22@gmail.com <sejalpore22@gmail.com> Co-authored-by: AbhiKora <AbhiKora@users.noreply.github.com>

--- Edberg/split.js
+++ Edberg/split.js
@@ -25,8 +25,8 @@ let htmlcssArrow = document.querySelector(".htmlcss-arrow");
 htmlcssArrow.onclick = function() {
  navLinks.classList.toggle("show1");
 }
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
+let moreArrow = document.querySelector(".more");
+more.onclick = function() {
  navLinks.classList.toggle("show2");
 }
 let jsArrow = document.querySelector(".js-arrow");
