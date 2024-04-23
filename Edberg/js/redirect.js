907657b4b12a0b46b88cac063b46d4a051929a19
AbhiKora <abhikora13@gmail.com>
2024-04-23 11:19:45 +0530

    redirecting

---
+++ Edberg/js/redirect.js
@@ -0,0 +1,59 @@
+document.getElementById("AIML").addEventListener("click", function() {
+    window.open("/Edberg/aimlcourse.html", "_blank");
+});
+
+document.getElementById("DS").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("DA").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("Cyber").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("DevOps").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("DigiMarkCom").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("DSA").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("FullStack").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("HTML").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("Java").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("MySQL").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("Python").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("Salesforce").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("SQLDev").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
+
+document.getElementById("UIUX").addEventListener("click", function() {
+    window.open("#", "_blank");
+});
